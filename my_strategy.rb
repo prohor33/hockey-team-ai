9fc62d47938cae3881d839d41ccb3aa2a0380397
Gladkikh Prohor <prohor33@gmail.com>
2014-09-16 01:26:17 +0400

    it's moving

--- my_strategy.rb
+++ my_strategy.rb
@@ -3,6 +3,7 @@ require './model/game'
 require './model/move'
 require './model/hockeyist'
 require './model/world'
+require './model/unit'
 
 class MyStrategy
   # @param [Hockeyist] me
@@ -10,10 +11,24 @@ class MyStrategy
   # @param [Game] game
   # @param [Move] move
   def move(me, world, game, move)
-    move.speed_up = -1.0
-    move.turn = Math::PI
+    # move.speed_up = -1.0
+    # move.turn = Math::PI
     # move.action = ActionType::STRIKE
 
-    Player p = world.getOpponentPlayer();
+    opp_player = world.get_opponent_player
+    # opp_player.neck.
+    
+    if (world.puck.owner_player_id == me.id)
+      # puck is mine now!
+      print "puck is mine now"
+      #move.turn = opp_player.net.center
+      move.turn = Math::PI
+    else
+      # need to get puck back
+      move.speed_up = 1.0
+      move.turn = me.get_angle_to_unit(world.puck)
+      move.action = ActionType::TAKE_PUCK
+    end
+ 
   end
 end
