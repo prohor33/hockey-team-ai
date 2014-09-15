6328d5ecfa32d497c97d35e4f79e24863509cbc4
Gladkikh Prohor <prohor33@gmail.com>
2014-09-16 01:43:41 +0400

    it's even shhoting now

--- my_strategy.rb
+++ my_strategy.rb
@@ -6,6 +6,9 @@ require './model/world'
 require './model/unit'
 
 class MyStrategy
+  
+  STRIKE_ANGLE = 1.0 * Math::PI / 180.0
+ 
   # @param [Hockeyist] me
   # @param [World] world
   # @param [Game] game
@@ -16,13 +19,24 @@ class MyStrategy
     # move.action = ActionType::STRIKE
 
     opp_player = world.get_opponent_player
+    my_player = world.get_my_player
     # opp_player.neck.
     
-    if (world.puck.owner_player_id == me.id)
+    if (world.puck.owner_player_id == my_player.id)
       # puck is mine now!
-      print "puck is mine now"
+      # print "puck is mine now"
       #move.turn = opp_player.net.center
       move.turn = Math::PI
+      
+      net_x = 0.5 * (opp_player.net_back + opp_player.net_front)
+      net_y = 0.5 * (opp_player.net_bottom + opp_player.net_top)
+      
+      angle_to_net = me.get_angle_to(net_x, net_y) 
+      move.turn = angle_to_net
+      
+      if (angle_to_net.abs < STRIKE_ANGLE)
+        move.action = ActionType::STRIKE
+      end
     else
       # need to get puck back
       move.speed_up = 1.0
